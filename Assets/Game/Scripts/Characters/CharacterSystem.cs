6c87f5a15ee5cf49ec22b27f87d430780c01352f
tmasten3 <tyler.masten00@gmail.com>
2025-11-13 18:27:26 -0600

    Fix Unity API usages causing compilation failures

--- Assets/Game/Scripts/Characters/CharacterSystem.cs
+++ Assets/Game/Scripts/Characters/CharacterSystem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Game.Core;
+using Logger = Game.Core.Logger;
 using Game.Data.Characters;
 using Game.Systems.Characters;
 using Game.Systems.EventBus;
--- Assets/Game/Scripts/Systems/BirthSystem.cs
+++ Assets/Game/Scripts/Systems/BirthSystem.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Game.Core;
+using UnityEngine;
 using Game.Data.Characters;
 using Game.Systems.EventBus;
 using Game.Systems.Population;
